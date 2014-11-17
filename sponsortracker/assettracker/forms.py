ce11b4a222550121581714016112d9beaa5ea64c
Austin <metalnut4@netscape.net>
2014-11-17 04:57:18 +0000

    Began working on basic validation of uploaded images.

--- sponsortracker/assettracker/forms.py
+++ sponsortracker/assettracker/forms.py
@@ -1,16 +1,48 @@
 import flask_wtf
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import HiddenField, SelectField, StringField, TextAreaField
-from wtforms.validators import InputRequired, Optional, URL
+from wtforms.validators import InputRequired, Optional, URL, ValidationError
+
+from wand.image import Image
+from PIL import Image as PillowImage
 
 from sponsortracker import data
 from sponsortracker.assettracker.app import asset_uploader
 
 _ASSET_CHOICES = [(type.name, type.label) for type in sorted(data.AssetType, key=lambda type: type.label)]
 
+
+def validate_asset(form, field):
+    image = Image(file=field.data.stream)
+    spec = data.AssetType[form.type.data].spec
+    
+    print("SIZE: " + str(image.size))
+    print("FORMAT: " + str(image.format))
+    print("COLORSPACE: " + str(image.colorspace))
+    print("RESOLUTION: " + str(image.resolution))
+    print("ALPHA: " + str(image.alpha_channel))
+    
+    if image.format not in spec.formats:
+        raise ValidationError("Unexpected file format. Expected: {0}, but is {1}.".format(spec.formats_as_str(), image.format))
+    if image.colorspace in spec.color_modes:
+        raise ValidationError("Unexpected color mode. Expected: {0}, but is {1}".format(spec.color_modes_as_str(), image.colorspace))
+    if image.resolution == (spec.dpi, spec.dpi):
+        raise ValidationError("Expected image resolution to be {0} dpi, but is {1} dpi.")
+    
+    if spec.transparent and not image.alpha_channel:
+        raise ValidationError("Expected a transparent background.")
+    elif spec.transparent is False and image.alpha_channel:
+        raise ValidationError("Expected an opaque background.")
+    
+    if image.width == spec.width and image.height == spec.height:
+        pass
+    
+    raise ValidationError("Validation succeeded")
+
+
 class UploadAssetForm(flask_wtf.Form):
     type = SelectField(choices=_ASSET_CHOICES, validators=[InputRequired("You must select an asset type.")])
-    asset = FileField(validators=[FileRequired(), FileAllowed(asset_uploader, "The uploaded asset must be an image file.")])
+    asset = FileField(validators=[FileRequired(), FileAllowed(asset_uploader, "The uploaded asset must be an image file."), validate_asset])
     
 class LinkForm(flask_wtf.Form):
     form_type = HiddenField(default="LinkForm")
--- sponsortracker/data.py
+++ sponsortracker/data.py
@@ -1,66 +1,81 @@
 from enum import Enum
 
 class _Format(Enum):
-    def __init__(self, ext):
+    def __init__(self, name, ext):
+        self.format = format
         self.ext = ext
 
 class _PrintFormat(_Format):
-    PDF = "pdf"
-    PSD = "psd"
-    AI = "al"
+    PDF = ("PDF", "pdf")
+    PSD = ("PSD", "psd")
 
 class _DigitalFormat(_Format):
-    JPG = "jpg"
-    PNG = "png"
-    GIF = "gif"
+    JPG = ("JPEG", "jpg")
+    PNG = ("PNG", "png")
+    GIF = ("GIF", "gif")
 
 class _LogoFormat(_Format):
-    EPS = "eps"
+    EPS = ("EPS", "eps")
 
 ASSET_FORMATS_EXT = [fmt.ext for fmt in list(_PrintFormat) + list(_DigitalFormat) + list(_LogoFormat)]
 
 class _ColorMode(Enum):
-    RGB = "RGB"
-    CMYK = "CMYK"
-    BW = "Black & White"
+    RGB = "rgb"
+    SRGB = "srgb"
+    CMYK = "cmyk"
+    BW = "gray"
 
 class _DimUnits(Enum):
     IN = "in"
     PX = "px"
 
 class ImageSpec:
-    def __init__(self, dpi, color_mode, width, height, unit, formats):
+    def __init__(self, dpi, width, height, unit, transparent, color_modes, formats, transparent=None):
         self.dpi = dpi
-        self.color_mode = color_mode
         self.width = width
         self.height = height
         self.unit = unit
+        self.color_modes = color_modes
         self.formats = formats
+        self.transparent = transparent
+    
+    def dimensions_as_str(self):
+        return "{width}{unit}x{height}{unit}".format(width=self.width, height=self.height, unit=self.unit)
+        
+    def formats_as_str(self):
+        return '/'.join([format.format for format in self.formats])
+        
+    def color_modes_as_str(self):
+        return '/'.join([mode.value for mode in self.color_modes])
+        
+    def dpi_as_str(self):
+        return "{0}".format(self.dpi)
     
     def __str__(self):
-        dimensions_str = "Dimensions: {width}{unit}x{height}{unit}".format(width=self.width, height=self.height, unit=self.unit)
-        formats_str = "Formats: {0}".format('/'.join([format.name for format in formats]))
-        color_mode_str = "Color Mode: {0}".format(color_mode=self.color_mode)
-        dpi_str = "{0}dpi".format(self.dpi)
-        return "; ".join([dimensions_str, formats_str, color_mode_str, dpi_str])
-
-class TextSpec:
-    pass
-
+        dimensions_str = "Dimensions: {0}".format(self.dimensions_as_str())
+        formats_str = "Formats: {0}".format(self.formats_as_str())
+        color_modes_str = "Color Modes: {0}".format(self.color_mode_as_str())
+        dpi_str = "{0} dpi".format(self.dpi_as_str())
+        return "; ".join([
+            "Dimensions: " self.dimensions_as_str(),
+            self.formats_as_str(),
+            self.color_mode_as_str(),
+            self.dpi_as_str()
+        ])
 
 # Each value should also contain details about each asset's required specs
 class AssetType(Enum):
-    DIGITAL_BANNER = ("Digital Guide - Banner", ImageSpec(72, _ColorMode.RGB, 640, 240, _DimUnits.PX, [_DigitalFormat.PNG]))
-    DIGITAL_MENU = ("Digital Guide - Menu", ImageSpec(72, _ColorMode.RGB, 600, 110, _DimUnits.PX, [_DigitalFormat.PNG]))
-    LOGO = ("Logo", ImageSpec(300, _ColorMode.RGB, 150, 150, _DimUnits.PX, [_PrintFormat.AI, _LogoFormat.EPS, _PrintFormat.PSD, _DigitalFormat.PNG]))
-    NEWSLETTER_HEADER = ("Newsletter - Header", ImageSpec(72, _ColorMode.RGB, 728, 90, _DimUnits.PX, list(_DigitalFormat)))
-    NEWSLETTER_SIDEBAR = ("Newsletter - Sidebar", ImageSpec(72, _ColorMode.RGB, 250, 300, _DimUnits.PX, list(_DigitalFormat)))
-    NEWSLETTER_FOOTER = ("Newsletter - Footer", ImageSpec(72, _ColorMode.RGB, 728, 90, _DimUnits.PX, list(_DigitalFormat)))
-    PROGRAM_QUARTER = ("Program - Quarter Page", ImageSpec(300, _ColorMode.BW, 5, 2, _DimUnits.IN, list(_PrintFormat)))
-    PROGRAM_HALF = ("Program - Half Page", ImageSpec(300, _ColorMode.BW, 5, 4, _DimUnits.IN, list(_PrintFormat)))
-    PROGRAM_WHOLE = ("Program - Single Page", ImageSpec(300, _ColorMode.CMYK, 5, 8, _DimUnits.IN, list(_PrintFormat)))
-    PROGRAM_DOUBLE = ("Program - Two Page", ImageSpec(300, _ColorMode.CMYK, 10.5, 8, _DimUnits.IN, list(_PrintFormat)))
-    WEBSITE_SIDEBAR = ("Website - Sidebar", ImageSpec(72, _ColorMode.RGB, 640, 240, _DimUnits.PX, [_DigitalFormat.PNG]))
+    DIGITAL_BANNER = ("Digital Guide - Banner", ImageSpec(72, 640, 240, _DimUnits.PX, [_ColorMode.RGB, _ColorMode.SRGB], [_DigitalFormat.PNG]))
+    DIGITAL_MENU = ("Digital Guide - Menu", ImageSpec(72, 600, 110, _DimUnits.PX, [_ColorMode.RGB, _ColorMode.SRGB], [_DigitalFormat.PNG]))
+    LOGO = ("Logo", ImageSpec(300, 150, 150, _DimUnits.PX, [_ColorMode.RGB, _ColorMode.SRGB], [_LogoFormat.EPS, _PrintFormat.PSD, _DigitalFormat.PNG], True))
+    NEWSLETTER_HEADER = ("Newsletter - Header", ImageSpec(72, 728, 90, _DimUnits.PX, [_ColorMode.RGB, _ColorMode.SRGB], list(_DigitalFormat)))
+    NEWSLETTER_SIDEBAR = ("Newsletter - Sidebar", ImageSpec(72, 250, 300, _DimUnits.PX,  [_ColorMode.RGB, _ColorMode.SRGB],list(_DigitalFormat)))
+    NEWSLETTER_FOOTER = ("Newsletter - Footer", ImageSpec(72, 728, 90, _DimUnits.PX, [_ColorMode.RGB, _ColorMode.SRGB], list(_DigitalFormat)))
+    PROGRAM_QUARTER = ("Program - Quarter Page", ImageSpec(300, 5, 2, _DimUnits.IN, [_ColorMode.BW], list(_PrintFormat)))
+    PROGRAM_HALF = ("Program - Half Page", ImageSpec(300, 5, 4, _DimUnits.IN, [_ColorMode.BW], list(_PrintFormat)))
+    PROGRAM_WHOLE = ("Program - Single Page", ImageSpec(300, 5, 8, _DimUnits.IN, [_ColorMode.CMYK], list(_PrintFormat)))
+    PROGRAM_DOUBLE = ("Program - Two Page", ImageSpec(300, 10.5, 8, _DimUnits.IN, [_ColorMode.CMYK], list(_PrintFormat)))
+    WEBSITE_SIDEBAR = ("Website - Sidebar", ImageSpec(72, 640, 240, _DimUnits.PX, [_ColorMode.RGB, _ColorMode.SRGB], [_DigitalFormat.PNG]))
     
     def __init__(self, label, spec):
         self.label = label
