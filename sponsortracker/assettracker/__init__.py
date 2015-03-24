2cacda1a59ee5a1f6e82ccaf33ad9bd4e261af9a
Austin <metalnut4@netscape.net>
2015-03-24 05:16:16 +0000

    Patching to account for Flask Uploads lack of support for Python 3 (uses dict.iteritems())

--- sponsortracker/assettracker/__init__.py
+++ sponsortracker/assettracker/__init__.py
@@ -4,6 +4,12 @@ from flaskext.uploads import configure_uploads, UploadSet
 from sponsortracker.assettracker.app import asset_uploader, preview_uploader, thumb_uploader
 from sponsortracker.app import app
 
+# Patch Flask Uploads lack of spport for Python 3
+class PatchedDict(dict):
+    def itervalues(self):
+        return self.values()
+app.upload_set_config = PatchedDict()
+
 configure_uploads(app, (asset_uploader, preview_uploader, thumb_uploader))
 
 from . import views
