4ba0f6765c8327d7e4f108dda3e7a0a8de34f427
Austin <metalnut4@netscape.net>
2015-04-08 23:47:31 +0000

    Tweaking migration, add users, and upload leads

--- scripts/add-users.py
+++ scripts/add-users.py
@@ -2,13 +2,16 @@ import os
 import random
 import string
 
-from sponsortracker import app, data, model
+from sponsortracker import data, model
 
 EMAIL_DELIM = ':'
 NAME_DELIM = ','
 USERS_DIR = "users/"
 
 def _add_user(type, first_name, last_name, username, email, password=None):
+    if model.UserAuth.query.filter_by(username=username).all():
+        return
+    
     password = password or ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
     
     email = model.UserEmail(email=email, is_primary=True)
@@ -17,11 +20,13 @@ def _add_user(type, first_name, last_name, username, email, password=None):
     user.emails.append(email)
     model.db.session.add(user)
     
+    print("Added {0} {1} ({2})".format(first_name, last_name, username))
+    
 def _import_users():
     for user_filename in os.listdir(USERS_DIR):
         type = os.path.splitext(user_filename)[0]
         with open(os.path.join(USERS_DIR, user_filename)) as user_file:
-            for user_line in user_file:
+            for user_line in user_file.readlines():
                 name, email = user_line.split(EMAIL_DELIM)
                 last_name, first_name = name.split(NAME_DELIM)
                 username = email.split('@')[0]
