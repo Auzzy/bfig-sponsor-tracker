dd1ad9ba56d0e0cefe5967a41c359a0b96cf78c7
Austin <metalnut4@netscape.net>
2015-05-01 01:18:19 +0000

    Tweaks from reverting

--- sponsortracker/dealtracker/views/core.py
+++ sponsortracker/dealtracker/views/core.py
@@ -27,7 +27,6 @@ def my_accounts():
     
 @deal_tracker.route("/all/")
 @login_required
->>>>>>> 0b6eaf4... Thumbnails are PNGs, regardless of the full image format. Can upload images without deal having a level set. Properly handles image formats not supported by the browser. Images are also stored in the database without their forlder.
 def all():
     return render_template("sponsor-list.html", sponsors=model.Sponsor.query.all())
 
@@ -106,17 +105,9 @@ def _configure_deal(id, form):
     else:
         deal = model.Deal(id, datetime.date.today().year, form.owner.data, form.cash.data, form.inkind.data, form.level_name.data)
     
-    print("DEAL YEAR: " + str(deal.year))
-    print("CURRENT DEAL YEAR: " + str(deal.current.year))
-    print(deal == deal.sponsor.current)
     if deal == deal.sponsor.current:
         deal.contract.ready = deal.cash > 0 or deal.inkind > 0
         deal.invoice.ready = deal.cash > 0 or deal.inkind > 0
-        
-        print("READY: " + str(deal.contract.ready))
-        print("SENT: " + str(deal.contract.sent))
-        print("RECEIVED: " + str(deal.contract.received))
-        
         deal.asset_request.ready = bool(deal.level_name) and deal.contract.received
         
     model.db.session.commit()
