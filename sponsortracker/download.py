af9449e00604b1ff01993c79345728d07f3d898c
Austin <metalnut4@netscape.net>
2015-09-03 00:15:08 +0000

    Bug

--- sponsortracker/download.py
+++ sponsortracker/download.py
@@ -29,10 +29,6 @@ def download(zipname=ZIPNAME, by_sponsor=True, info=True, asset_filter=lambda de
         zipdir = join(tempdir, zipname)
         os.makedirs(zipdir)
         
-        
-        COPPER, SILVER, GOLD, PLATINUM
-        
-        
         for deal in model.Deal.query.filter(model.Deal.level_name != ""):
             if deal.level not in (data.Level.COPPER, data.Level.SILVER, data.Level.GOLD, data.Level.PLATINUM) or deal.contract.received != None or deal.invoice.received != None:
                 if not level or deal.level_name == level:
